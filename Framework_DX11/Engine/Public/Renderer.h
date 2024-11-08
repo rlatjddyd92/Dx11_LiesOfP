0841ac2859285dfa9047b045b1015979339b3e8c
YangWooSong <90258668+YangWooSong@users.noreply.github.com>
2024-11-08 19:08:34 +0900

    .

--- Framework_DX11/Engine/Public/Renderer.h
+++ Framework_DX11/Engine/Public/Renderer.h
@@ -11,7 +11,7 @@ BEGIN(Engine)
 class CRenderer final : public CBase
 {
 public:
-	enum RENDERGROUP { RG_PRIORITY, RG_HEIGHT, RG_SHADOWOBJ, RG_NONBLEND, RG_NONLIGHT, RG_BLEND, RG_PICKING, RG_UI, RG_END };
+	enum RENDERGROUP { RG_PRIORITY, RG_HEIGHT, RG_SHADOWOBJ, RG_NONBLEND, RG_NONLIGHT, RG_BLEND, RG_PICKING, RG_UI, RG_DECAL, RG_END };
 private:
 	CRenderer(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
 	virtual ~CRenderer() = default;
@@ -84,6 +84,7 @@ private:
 	HRESULT Render_Height();
 	HRESULT Render_ShadowObj();
 	HRESULT Render_NonBlend();
+	HRESULT Render_Decal();
 	HRESULT Render_Lights();
 
 	HRESULT Render_Deferred();
