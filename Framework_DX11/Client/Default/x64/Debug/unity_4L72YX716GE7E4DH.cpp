374f022203c69cdace1b905b652d4ad63795fba5
C1wa <chorok1955@naver.com>
2024-12-05 23:09:02 +0900

    갓핸드 기초공사

--- Framework_DX11/Client/Default/x64/Debug/unity_4L72YX716GE7E4DH.cpp
+++ Framework_DX11/Client/Default/x64/Debug/unity_4L72YX716GE7E4DH.cpp
@@ -6,6 +6,9 @@
 #include "C:\Users\Administrator\Documents\GitHub\Dx11_LiesOfP\Framework_DX11\Client\Private\AObj_ChargeSwing.cpp"
 
 
+#include "C:\Users\Administrator\Documents\GitHub\Dx11_LiesOfP\Framework_DX11\Client\Private\AObj_GodHands.cpp"
+
+
 #include "C:\Users\Administrator\Documents\GitHub\Dx11_LiesOfP\Framework_DX11\Client\Private\AObj_GoldBall.cpp"
 
 
---
+++ Framework_DX11/Client/Public/AObj_GodHands.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "AttackObject.h"
+#include "Effect_Container.h"
+
+class CAObj_GodHands	final : public CAttackObject
+{
+protected:
+	CAObj_GodHands(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CAObj_GodHands(const CGameObject& Prototype);
+	virtual ~CAObj_GodHands() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+	virtual HRESULT Render_LightDepth() override;
+
+public:
+	virtual void OnCollisionEnter(CGameObject* pOther) override;
+	virtual void OnCollisionStay(CGameObject* pOther) override;
+	virtual void OnCollisionExit(CGameObject* pOther) override;
+
+private:
+	class CEffect_Container* m_pEffect = { nullptr };
+
+private:
+	HRESULT Ready_Components();
+
+public:
+	static CAObj_GodHands* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg);
+	virtual void Free() override;
+};
+
