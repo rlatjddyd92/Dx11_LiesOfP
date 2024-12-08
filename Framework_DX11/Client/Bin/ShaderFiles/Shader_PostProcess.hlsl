57d69eb9b2b17816a7e57714afaa4c62aaccb51a
SeongHyunJeong <sean3080211@gmail.com>
2024-12-08 10:33:55 +0900

    Merge

--- Framework_DX11/Client/Bin/ShaderFiles/Shader_PostProcess.hlsl
+++ Framework_DX11/Client/Bin/ShaderFiles/Shader_PostProcess.hlsl
@@ -31,13 +31,6 @@ texture2D g_EffectTexture;
 texture2D g_EffectBlur;
 
 
-static const float SampleWeights[13] =
-{
-    0.0561f, 0.1353f, 0.278f, 0.4868f, 0.7261f, 0.9231f, 1.f, 0.9231f, 0.7261f, 0.4868f, 0.278f, 0.1353f, 0.0561f
-    //0.002216, 0.008764, 0.026995, 0.064759, 0.120985, 0.176033, 0.199471, 0.176033, 0.120985, 0.064759, 0.026995, 0.008764, 0.002216,
-};
-
-
 struct VS_IN
 {
 	float3 vPosition : POSITION;	
@@ -188,19 +181,6 @@ PS_OUT PS_MAIN_RADIALBLUR(PS_IN In)
 
     //    // 텍스처 샘플링 및 누적
     //    vColor += g_BackTexture.Sample(LinearClampSampler, sampleUV);
-    //}
-    
-    //int iDivision = 0;
-    //for (uint i = 0; i < 20; ++i)
-    //{
-    //    float fScale = 1.f + ((float) i * (g_RadialPower * (1.0f / 19.f)));
-    //    float2 UV = vDirection * fScale + g_vRadialCenterPos;
-		
-    //    if (0.f > UV.x || 1.f < UV.x || 0.f > UV.y || 1.f < UV.y)
-    //        continue;
-
-    //    vColor += g_BackTexture.Sample(LinearClampSampler, UV);
-    //    ++iDivision;
     //}
 
     float2 vDiff = ((In.vTexcoord - g_vRadialCenterPos) * 0.5f);
