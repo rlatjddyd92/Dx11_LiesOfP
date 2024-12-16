427d683b0408b5fd2c7d5e5ea6cedba61e6eec72
YangWooSong <90258668+YangWooSong@users.noreply.github.com>
2024-12-16 20:24:13 +0900

    Add Player Arm Binary Dat

--- Binary_DX11/Client/Private/Loader.cpp
+++ Binary_DX11/Client/Private/Loader.cpp
@@ -97,6 +97,12 @@ HRESULT CLoader::Ready_Resources_For_GamePlayLevel()
 	//	CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../Bin/Resources/Models/Player/Player.fbx", "../Bin/ModelData/Anim/Player/", PreTransformMatrix))))
 	//	return E_FAIL;
 
+	/* For. Prototype_Component_Model_Player*/
+	PreTransformMatrix = XMMatrixRotationY(XMConvertToRadians(180.0f));
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Model_Player"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../Bin/Resources/Models/Player/Player_Arm.fbx", "../Bin/ModelData/Anim/Player/", PreTransformMatrix))))
+		return E_FAIL;
+	// 
 	//if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Model_Player_Test01"),
 	//	CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../Bin/Resources/Models/Player/Player_Test01.fbx", "../Bin/ModelData/Anim/Player/", PreTransformMatrix))))
 	//	return E_FAIL;
@@ -148,8 +154,8 @@ HRESULT CLoader::Ready_Resources_For_GamePlayLevel()
 	//if (FAILED(Ready_Resources_For_Weapon()))
 	//	return E_FAIL;
 
-	if (FAILED(Ready_Resources_CutScene()))
-		return E_FAIL;
+	//if (FAILED(Ready_Resources_CutScene()))
+	//	return E_FAIL;
 
 	lstrcpy(m_szLoadingText, TEXT("변환 완료"));
 
