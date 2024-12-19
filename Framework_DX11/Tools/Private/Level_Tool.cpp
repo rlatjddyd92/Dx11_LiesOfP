d4e7795a0880e6fd037e6f2925f7f77117b29371
KoJunHo123 <88295444+KoJunHo123@users.noreply.github.com>
2024-12-20 06:21:42 +0900

    Player_ArnSkill_1230_0621

--- Framework_DX11/Tools/Private/Level_Tool.cpp
+++ Framework_DX11/Tools/Private/Level_Tool.cpp
@@ -54,8 +54,8 @@ HRESULT CLevel_GamePlay::Initialize()
 		return E_FAIL;*/
 
 	//strcpy_s(Desc.szModelTag, "Prototype_Component_Model_FlameSword");
-	//strcpy_s(Desc.szModelTag, "Prototype_Component_Model_Player_Arm");
-	strcpy_s(Desc.szModelTag, "Prototype_Component_Model_Raxasia_Sword_CutScene");
+	strcpy_s(Desc.szModelTag, "Prototype_Component_Model_Player_Arm");
+	//strcpy_s(Desc.szModelTag, "Prototype_Component_Model_Raxasia_Sword_CutScene");
 	if (FAILED(m_pGameInstance->Add_CloneObject_ToLayer(LEVEL_TOOL, TEXT("Layer_Offset"), TEXT("Prototype_GameObject_NonAnim"), &Desc)))
 		return E_FAIL;
 	//if (FAILED(m_pGameInstance->Add_CloneObject_ToLayer(LEVEL_TOOL, TEXT("Layer_BackGround"), TEXT("Prototype_GameObject_CutSceneodHand"))))
