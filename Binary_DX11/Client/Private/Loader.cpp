30dbf2efa49c6d3f9b69a5ee34d1d326de61e88f
SeongHyun Jeong (정승현) <sean3080211@gmail.com>
2024-12-21 16:47:57 +0900

    Add SteelHeart dat

--- Binary_DX11/Client/Private/Loader.cpp
+++ Binary_DX11/Client/Private/Loader.cpp
@@ -127,9 +127,13 @@ HRESULT CLoader::Ready_Resources_For_GamePlayLevel()
 	//	CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../Bin/Resources/Models/Monastery_TheLastDoor/SK_FO_Monastery_TheLastDoor_01.fbx", "../Bin/ModelData/NonAnim/InteractObj/Tex/", PreTransformMatrix))))
 	//	return E_FAIL;
 
-	// 일반 몬스터 로드 함수
-	if (FAILED(Ready_Resources_For_BasicMonster()))
+	if (FAILED(m_pGameInstance->Add_Prototype(LEVEL_GAMEPLAY, TEXT("Prototype_Component_Model_SteelHeart"),
+		CModel::Create(m_pDevice, m_pContext, CModel::TYPE_ANIM, "../Bin/Resources/Models/SteelHeart/SteelHeart.fbx", "../Bin/ModelData/Anim/SteelHeart/", PreTransformMatrix))))
 		return E_FAIL;
+	 
+	// 일반 몬스터 로드 함수
+	//if (FAILED(Ready_Resources_For_BasicMonster()))
+	//	return E_FAIL;
 	 
 	//if (FAILED(Ready_Resources_For_BossMonster()))
 	//	return E_FAIL;
