72d4f8fc96e7246433bfd32299ec9a7008497ad6
rlatjddyd92 <rlatjddyd9@gmail.com>
2024-12-28 16:13:10 +0900

    튜토리얼 클래스 제작

---
+++ Framework_DX11/Client/Private/UIPage_Tutorial.cpp
@@ -0,0 +1,146 @@
+#include "stdafx.h"
+#include "..\Public\UIPage_Tutorial.h"
+
+#include "GameInstance.h"
+#include "GameInterface_Controller.h"
+
+CUIPage_Tutorial::CUIPage_Tutorial(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CUIPage{ pDevice, pContext }
+{
+
+}
+
+CUIPage_Tutorial::CUIPage_Tutorial(const CUIPage_Tutorial& Prototype)
+	: CUIPage{ Prototype }
+{
+}
+
+HRESULT CUIPage_Tutorial::Initialize_Prototype()
+{
+	// 모든 UIPage는 클론 안 함, 여기서 모든 세팅 끝내기 
+
+	UI_DESC			Desc{};
+
+	Desc.fX = g_iWinSizeX >> 1;
+	Desc.fY = g_iWinSizeY >> 1;
+	Desc.fSizeX = g_iWinSizeX;
+	Desc.fSizeY = g_iWinSizeY;
+
+	Desc.fSpeedPerSec = 10.f;
+	Desc.fRotationPerSec = XMConvertToRadians(90.0f);
+
+	m_vecPageAction.resize(_int(PAGEACTION::ACTION_END));
+
+	/* 직교퉁여을 위한 데이터들을 모두 셋하낟. */
+	if (FAILED(__super::Initialize(&Desc)))
+		return E_FAIL;
+
+	m_fTopPartMove = -1.f;
+
+	return S_OK;
+}
+
+HRESULT CUIPage_Tutorial::Initialize(void* pArg)
+{
+
+
+	return S_OK;
+}
+
+void CUIPage_Tutorial::Priority_Update(_float fTimeDelta)
+{
+	__super::Priority_Update(fTimeDelta);
+}
+
+void CUIPage_Tutorial::Update(_float fTimeDelta)
+{
+	__super::Update(fTimeDelta);
+}
+
+void CUIPage_Tutorial::Late_Update(_float fTimeDelta)
+{
+	__super::Late_Update(fTimeDelta);
+
+	for (auto& iter : m_vec_Group_Ctrl)
+		__super::UpdatePart_ByControl(iter);
+}
+
+HRESULT CUIPage_Tutorial::Render()
+{
+	return S_OK;
+}
+
+void CUIPage_Tutorial::OpenAction()
+{
+	__super::OpenAction();
+}
+
+void CUIPage_Tutorial::CloseAction()
+{
+	__super::CloseAction();
+}
+
+CHECK_MOUSE CUIPage_Tutorial::Check_Page_Action(_float fTimeDelta)
+{
+	__super::Check_Page_Action(fTimeDelta);
+
+	return CHECK_MOUSE::MOUSE_NONE;
+}
+
+HRESULT CUIPage_Tutorial::Ready_UIPart_Group_Control()
+{
+	__super::Ready_UIPart_Group_Control();
+
+	m_vec_Group_Ctrl.resize(_int(PART_GROUP::GROUP_END));
+
+	for (auto& iter : m_vec_Group_Ctrl)
+		iter = new UG_CTRL;
+
+	for (_int i = 0; i < m_vecPart.size(); ++i)
+	{
+		if (m_vecPart[i]->iGroupIndex != -1)
+			m_vec_Group_Ctrl[m_vecPart[i]->iGroupIndex]->PartIndexlist.push_back(i);
+	}
+
+	m_bRender = false;
+
+	return S_OK;
+}
+
+CUIPage_Tutorial* CUIPage_Tutorial::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CUIPage_Tutorial* pInstance = new CUIPage_Tutorial(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX(TEXT("Failed to Created : CUIPage_Tutorial"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject* CUIPage_Tutorial::Clone(void* pArg)
+{
+	CUIPage_Tutorial* pInstance = new CUIPage_Tutorial(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX(TEXT("Failed to Cloned : CUIPage_Tutorial"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CUIPage_Tutorial::Free()
+{
+	__super::Free();
+
+	for (auto& iter : m_vecPart)
+	{
+		Safe_Delete(iter);
+	}
+
+	m_vecPart.clear();
+}
---
+++ Framework_DX11/Client/Public/UIPage_Tutorial.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "UIPage.h"
+
+BEGIN(Engine)
+
+END
+
+BEGIN(Client)
+class CUITutorial_Info;
+class CUITutorial_Popup;
+class CUITutorial_Timing;
+class CUITutorial_Result;
+
+class CUIPage_Tutorial : public CUIPage
+{
+public:
+	enum class PART_GROUP
+	{
+		GROUP_FRAME,
+		GROUP_INFO,
+		GROUP_POPUP,
+		GROUP_TIMING,
+		GROUP_RESULT,
+		GROUP_END
+	};
+
+
+
+
+protected:
+	CUIPage_Tutorial(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CUIPage_Tutorial(const CUIPage_Tutorial& Prototype);
+	virtual ~CUIPage_Tutorial() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+
+	virtual void OpenAction() override;
+	virtual void CloseAction() override;
+
+	virtual CHECK_MOUSE Check_Page_Action(_float fTimeDelta) override;
+
+public:
+	const vector<UPART*>& Get_UIPartInfo() { return m_vecPart; }
+	virtual HRESULT Ready_UIPart_Group_Control() override;
+
+protected:
+	class CUITutorial_Info* m_pSharedPointer_Info = { nullptr };
+	class CUITutorial_Popup* m_pSharedPointer_Popup = { nullptr };
+	class CUITutorial_Timing* m_pSharedPointer_Timing = { nullptr };
+	class CUITutorial_Result* m_pSharedPointer_Result = { nullptr };
+
+
+
+
+public:
+	static CUIPage_Tutorial* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg);
+	virtual void Free() override;
+
+
+
+
+};
+
+END
