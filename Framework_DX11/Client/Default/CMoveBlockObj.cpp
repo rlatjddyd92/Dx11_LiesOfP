59fe2d8c9285ab9d67533b05feb2c6edc9be6bc5
YangWooSong <90258668+YangWooSong@users.noreply.github.com>
2025-01-03 10:07:56 +0900

    MoveBlock

--- Framework_DX11/Client/Default/CMoveBlockObj.cpp
+++ Framework_DX11/Client/Default/CMoveBlockObj.cpp
@@ -1,2 +1,79 @@
 #include "stdafx.h"
 #include "CMoveBlockObj.h"
+#include "GameInstance.h"
+
+CMoveBlockObj::CMoveBlockObj(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+    : CGameObject{ pDevice, pContext }
+{
+}
+
+CMoveBlockObj::CMoveBlockObj(const CMoveBlockObj& Prototype)
+    : CGameObject{ Prototype }
+{
+}
+
+HRESULT CMoveBlockObj::Initialize_Prototype()
+{
+    return S_OK;
+}
+
+HRESULT CMoveBlockObj::Initialize(void* pArg)
+{
+	MOVEBLOCK_DESC* pDesc = static_cast<MOVEBLOCK_DESC*>(pArg);
+
+	if (FAILED(__super::Initialize(pArg)))
+		return E_FAIL;
+
+    if (FAILED(Ready_Components(pDesc)))
+        return E_FAIL;
+
+	return S_OK;
+}
+
+void CMoveBlockObj::Priority_Update(_float fTimeDelta)
+{
+}
+
+void CMoveBlockObj::Update(_float fTimeDelta)
+{
+}
+
+void CMoveBlockObj::Late_Update(_float fTimeDelta)
+{
+}
+
+HRESULT CMoveBlockObj::Render()
+{
+    return E_NOTIMPL;
+}
+
+void CMoveBlockObj::OnCollisionEnter(CGameObject* pOther)
+{
+}
+
+void CMoveBlockObj::OnCollisionStay(CGameObject* pOther)
+{
+}
+
+void CMoveBlockObj::OnCollisionExit(CGameObject* pOther)
+{
+}
+
+HRESULT CMoveBlockObj::Ready_Components(MOVEBLOCK_DESC* pDesc)
+{
+    return E_NOTIMPL;
+}
+
+CMoveBlockObj* CMoveBlockObj::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+    return nullptr;
+}
+
+CGameObject* CMoveBlockObj::Clone(void* pArg)
+{
+    return nullptr;
+}
+
+void CMoveBlockObj::Free()
+{
+}
--- Framework_DX11/Client/Default/CMoveBlockObj.h
+++ Framework_DX11/Client/Default/CMoveBlockObj.h
@@ -1,7 +1,54 @@
 #pragma once
-#include "C:\Users\olivi\GitHub\Dx11_LiesOfP\Framework_DX11\EngineSDK\Inc\GameObject.h"
+#include "Client_Defines.h"
+#include "GameObject.h"
+
+BEGIN(Engine)
+class CCollider;
+class CRigidBody;
+END
+
+BEGIN(Client)
+
 class CMoveBlockObj :
     public CGameObject
 {
+public:
+	enum	BLOCKTYPE{RAXASIA1, RAXASIA2, MANUS1, TYPE_END};
+
+	typedef struct : public CGameObject::GAMEOBJECT_DESC
+	{
+		_uint		iTypeNum = { 0 };
+	}MOVEBLOCK_DESC;
+
+private:
+	CMoveBlockObj(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	CMoveBlockObj(const CMoveBlockObj& Prototype);
+	virtual ~CMoveBlockObj() = default;
+
+public:
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+	virtual void Priority_Update(_float fTimeDelta) override;
+	virtual void Update(_float fTimeDelta) override;
+	virtual void Late_Update(_float fTimeDelta) override;
+	virtual HRESULT Render() override;
+
+public:
+	virtual void OnCollisionEnter(CGameObject* pOther) override;
+	virtual void OnCollisionStay(CGameObject* pOther) override;
+	virtual void OnCollisionExit(CGameObject* pOther) override;
+
+private:
+	CCollider* m_pColliderCom = { nullptr };
+	CRigidBody* m_pRigidBodyCom = { nullptr };
+
+private:
+	HRESULT Ready_Components(MOVEBLOCK_DESC* pDesc);
+
+public:
+	static CMoveBlockObj* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CGameObject* Clone(void* pArg);
+	virtual void Free() override;
 };
 
+END
